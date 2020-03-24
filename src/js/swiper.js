9af5f85d0fd299f04ff8641193fdec702ce94a79
chomeko3o <kusyopi@gmail.com>
2020-03-25 01:01:25 +0900

    maivisual仮終了

---
+++ src/js/swiper.js
@@ -0,0 +1,19 @@
+$( function() {
+  var swiper = new Swiper('.swiper__container', {
+    speed: 400,
+    autoplay: true,
+    loop: true,
+    effect: 'fade',
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+    on: {
+      slideChange: function () {
+        jQuery('.swiper-slide-content').css('opacity', '0');
+        realIndex = this.realIndex + 1;
+        jQuery('.swiper-slide-content-' + realIndex).css('opacity', '1');
+      }
+    },
+  });
+});
