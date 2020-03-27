d29bf5ac41f8c2fc5829ba72deac9fa153da8656
chomeko3o <kusyopi@gmail.com>
2020-03-27 23:18:04 +0900

    swiper途中経過

--- src/js/swiper.js
+++ src/js/swiper.js
@@ -17,3 +17,31 @@ $( function() {
     },
   });
 });
+
+$(function () {
+  var swiper = new Swiper('.slider2', {
+    speed: 400,
+    loop: true,
+    //spaceBetween: 35,
+    slidesPerView: 1.4,
+    centeredSlides: true,
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    breakpoints: {
+      //990: {
+        //slidesPerView: 2,
+        //spaceBetween: 0,
+      //},
+      768: {
+        slidesPerView: 1.4,
+        //spaceBetween: 30,
+      },
+    },
+  });
+});
