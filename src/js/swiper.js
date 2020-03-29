e3a174c6b4afa31c33c493905cd5d4ae96ed7e4f
chomeko3o <kusyopi@gmail.com>
2020-03-29 15:18:08 +0900

    swiper　完成

--- src/js/swiper.js
+++ src/js/swiper.js
@@ -16,5 +16,28 @@ $( function() {
       }
     },
   });
+
+  var swiper = new Swiper('.slider2', {
+    speed: 400,
+    loop: true,
+    spaceBetween: 10,
+    slidesPerView: 2,
+    centeredSlides: true,
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+    breakpoints: {
+      990: {
+        slidesPerView: 1,
+        spaceBetween: 10,
+        //centeredSlides : true,
+      },
+    }
+  });
 });
 
